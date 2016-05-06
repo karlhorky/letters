d8a445fa6ff70e404a19289826c49e4cf8317b52
Helen V. Holmes <helen.v.holmes@gmail.com>
2016-05-06 09:00:14 -0400

    Working on sass middleware

--- server-bullshit/app.js
+++ server-bullshit/app.js
@@ -1,16 +1,24 @@
-var bodyParser   = require('body-parser');
-var cookieParser = require('cookie-parser');
-var express      = require('express');
-var favico       = require('serve-favicon');
-var logger       = require('morgan');
-var marked       = require('marked');
-var nunjucks     = require('nunjucks')
-var path         = require('path');
+var bodyParser     = require('body-parser');
+var cookieParser   = require('cookie-parser');
+var express        = require('express');
+var favicon        = require('serve-favicon');
+var logger         = require('morgan');
+var marked         = require('marked');
+var nunjucks       = require('nunjucks')
+var path           = require('path');
+var sassMiddleware = require('node-sass-middleware');
 
 var routes = require('./routes/index');
 var letters = require('./routes/letters');
 
 var app = express();
+
+if (app.get('env') == 'development') {
+  var browserSync = require('browser-sync');
+  var bs = browserSync({ logSnippet: false });
+  app.use(require('connect-browser-sync')(bs));
+}
+
 
 // view engine setup
 var nunjucksEnv = nunjucks.configure('views', {
@@ -19,11 +27,20 @@ var nunjucksEnv = nunjucks.configure('views', {
 });
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'styles'),
+  dest: path.join(__dirname, 'public'),
+  debug: true,
+  outputStyle: 'compressed',
+  prefix:  '/stylesheets'  // Where prefix is at <link rel="stylesheets" href="stylesheets/style.css"/>
+}));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
@@ -54,7 +71,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  nunjucks.render('error.html', {
+  nunjucks.render('error/error.html', {
     message: err.message,
     error: {}
   });
