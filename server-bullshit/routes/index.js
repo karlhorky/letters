daab671b427ccd4c4805487b9eed546d774e8196
Helen V. Holmes <helen.v.holmes@gmail.com>
2016-06-08 12:08:58 -0400

    ???

--- server-bullshit/routes/index.js
+++ server-bullshit/routes/index.js
@@ -4,7 +4,7 @@ var nunjucks = require('nunjucks')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home/index2.html', { title: 'Express' });
+  res.render('home/index.html', { title: 'Express' });
 });
 
 module.exports = router;
