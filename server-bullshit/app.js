2c1b86c476f9ec0f5fbcfe85a3df9f3d57b8648c
Helen V. Holmes <helen.v.holmes@gmail.com>
2016-06-28 07:51:41 -0400

    Starting on the long form section, designing the letters/ page

--- server-bullshit/app.js
+++ server-bullshit/app.js
@@ -11,6 +11,7 @@ var sassMiddleware = require('node-sass-middleware');
 var routes = require('./routes/index');
 var letters = require('./routes/letters');
 var experiments = require('./routes/experiments');
+var writing = require('./routes/writing');
 
 var app = express();
 
@@ -32,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/letters', letters);
 app.use('/experiments', experiments);
+app.use('/writing', writing);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
