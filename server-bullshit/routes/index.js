300e21d70d83925bb3dfafd1278caf795511ea8c
Helen V. Holmes <helen.v.holmes@gmail.com>
2016-08-21 16:22:29 -0400

    Who knows, yo

--- server-bullshit/routes/index.js
+++ server-bullshit/routes/index.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var router = express.Router();
-var nunjucks = require('nunjucks')
+var nunjucks = require('nunjucks');
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+var randomNumber = getRandomInt(1, 3);
+
+console.log(randomNumber);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home/index.html', { title: 'Helen V. Holmes' });
+  res.render('home/index' + [randomNumber] + '.html', { title: 'Helen V. Holmes' });
 });
 
 module.exports = router;
