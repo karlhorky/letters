7658eb217b3b132d3a4a17b401fb932e51b63d72
Helen V. Holmes <helen.v.holmes@gmail.com>
2017-01-16 10:55:49 -0500

    Bllleeehhhh

---
+++ server/public/javascripts/neuehaasunica.js
@@ -0,0 +1,19 @@
+FontFaceOnload( "NeueHaasUnica", {
+  success: function() {
+    var docEl = document.documentElement;
+
+    // Stage 1 Complete
+    // FOFT engaged
+    docEl.className += " neuehaasunica-loaded";
+  }
+});
+
+FontFaceOnload( "LibreCaslonDisplay", {
+  success: function() {
+    var docEl = document.documentElement;
+
+    // Stage 1 Complete
+    // FOFT engaged
+    docEl.className += " librecaslon-loaded";
+  }
+});
