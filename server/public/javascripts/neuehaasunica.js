68928777ee03343d5c114749dad37549f83624a0
Helen V. Holmes <helen.v.holmes@gmail.com>
2017-01-16 20:27:13 -0500

    Start home page

--- server/public/javascripts/neuehaasunica.js
+++ server/public/javascripts/neuehaasunica.js
@@ -17,3 +17,23 @@ FontFaceOnload( "LibreCaslonDisplay", {
     docEl.className += " librecaslon-loaded";
   }
 });
+
+FontFaceOnload( "AvenirNext", {
+  success: function() {
+    var docEl = document.documentElement;
+
+    // Stage 1 Complete
+    // FOFT engaged
+    docEl.className += " avenirnext-loaded";
+  }
+});
+
+FontFaceOnload( "AvenirNextBold", {
+  success: function() {
+    var docEl = document.documentElement;
+
+    // Stage 1 Complete
+    // FOFT engaged
+    docEl.className += " avenirnext-bold-loaded";
+  }
+});
