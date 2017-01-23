707eba52e4d7b134066498e395767924b7e07960
Helen V. Holmes <helen.v.holmes@gmail.com>
2017-01-23 18:46:44 -0500

    Unclear why subroutes in currently aren't working

--- server/routes/currently.js
+++ server/routes/currently.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var router = express.Router();
-var nunjucks = require('nunjucks')
+var nunjucks = require('nunjucks');
+
+var pathToLongFormContent = "./views/currently/";
+var shorturls = [];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('currently/currently.html', { title: 'Currently Collections' });
+});
+
+router.get('/march-2015', function(req, res, next) {
+  res.render('currently/march-2015/march-2015.html', { title: 'Currently Collections' });
+});
+
+router.get('/january-2014', function(req, res, next) {
+  res.render('currently/january-2014/january-2014.html', { title: 'Currently Collections' });
 });
 
 module.exports = router;
