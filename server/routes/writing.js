466f34fde1fb9c3a897a6a98b7210718b3b0a1a3
Helen V. Holmes <helen.v.holmes@gmail.com>
2017-02-21 17:45:45 -0500

    Ugh jesus

--- server/routes/writing.js
+++ server/routes/writing.js
@@ -7,11 +7,13 @@ var glob     = require("glob");
 
 var pathToLongFormContent = "./views/writing/";
 var shorturls = [];
+var shorturlTitles = [];
 
 var files = glob.sync("**/*.html", {cwd: pathToLongFormContent});
 
 function indexLongformWriting() {
   var shorturl;
+  var shorturlTitle;
 
   files.forEach(file => {
     if (file === "writing.html") {
@@ -21,6 +23,12 @@ function indexLongformWriting() {
     shorturl = file.match(/(.*)\.html$/)[1];
     shorturl = shorturl.substring(shorturl.indexOf("/") + 1);
     shorturls.push(shorturl);
+
+    shorturls = shorturls.reverse();
+
+    shorturlTitle = shorturl.substring(6);
+    shorturlTitles.push(shorturlTitle);
+    shorturlTitles = shorturlTitles.reverse();
   });
 }
 
@@ -30,22 +38,24 @@ indexLongformWriting();
 router.get('/', function(req, res, next) {
   res.render('writing/writing.html', { 
     title: 'Writing',
-    longFormTitles: shorturls
+    longFormTitles: shorturlTitles
   });
 });
 
 shorturls.forEach(shorturl => {
-  if (shorturl === "type-is-your-right") {
-    router.get("/" + shorturl, function(req, res, next) {
-      res.render("writing/" + shorturl + "/" + shorturl + ".html", { title: 'type is your right' });
+  var shorturlTitle = shorturl.substring(6);
+
+  if (shorturl === '16-08-type-is-your-right') {
+    router.get('/' + shorturlTitle, function(req, res, next) {
+      res.render('writing/' + shorturlTitle + '/' + shorturl + '.html', { title: 'Type is Your Right!' });
     });
-  } else if (shorturl === "baas") {
-    router.get("/" + shorturl, function(req, res, next) {
-      res.render("writing/" + shorturl + "/" + shorturl + ".html", { title: 'baas' });
+  } else if (shorturl === '15-11-baas') {
+    router.get('/' + shorturlTitle, function(req, res, next) {
+      res.render('writing/' + shorturlTitle + '/' + shorturl + '.html', { title: 'Baas' });
     });
-  } else { // most articles are "the"-something
-    router.get("/the-" + shorturl, function(req, res, next) {
-      res.render("writing/" + shorturl + "/" + shorturl + ".html", { title: shorturl });
+  } else { // most articles are 'the'-something
+    router.get('/the-' + shorturlTitle, function(req, res, next) {
+      res.render('writing/' + shorturlTitle + '/' + shorturl + '.html', { title: shorturlTitle.replace(/-/g, ' ') });
     });
   }
 });
